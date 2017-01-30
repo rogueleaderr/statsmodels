Fixes to tests to accomodate cell signs

statsmodels/stats/tests/test_contingency_tables.py
@@ -592,11 +592,12 @@ def test_calculate_pairwise_chi2s_for_MMI_item_response_table():
     multiple_response_table = ctab.MultipleResponseTable([rows_factor, ],
                                                          [columns_factor])
     calculate = multiple_response_table._chi2s_for_MMI_item_response_table
-    pairwise_chis = calculate(rows_factor, columns_factor)
+    pairwise_chis, cell_signs = calculate(rows_factor, columns_factor)
     r_results_fname = "srcv_r_all_chis_result.csv"
     r_results_fpath = os.path.join(results_dirpath, r_results_fname)
     results_from_r = pd.Series.from_csv(r_results_fpath)
     assert_allclose(pairwise_chis, results_from_r)
+    np.testing.assert_(cell_signs.iat[0,0] == -1)
 
 
 def test_multiple_mutual_independence_false_using_bonferroni():
@@ -609,8 +610,8 @@ def test_multiple_mutual_independence_false_using_bonferroni():
     multiple_response_table = ctab.MultipleResponseTable([rows_factor, ],
                                                          [columns_factor])
     bonferroni_test = multiple_response_table._test_MMI_using_bonferroni
-    p_value_overall, p_values_cellwise = bonferroni_test(rows_factor,
-                                                       columns_factor)
+    results = bonferroni_test(rows_factor, columns_factor)
+    p_value_overall, p_values_cellwise, cellwise_signs = results
     fpath = os.path.join(results_dirpath, "srcv_r_bonferroni.csv")
     r_result = pd.DataFrame.from_csv(fpath)
     p_value_overall_r = r_result["p.value.bon"]
@@ -647,12 +648,14 @@ def test_calculate_pairwise_chi2s_for_SPMI_item_response_table():
     multiple_response_table = ctab.MultipleResponseTable([rows_factor, ],
                                                          [columns_factor])
     calculate = multiple_response_table._chi2s_for_SPMI_item_response_table
-    spmi_pairwise_chis_python = calculate(rows_factor, columns_factor)
+    results = calculate(rows_factor, columns_factor)
+    spmi_pairwise_chis_python, cellwise_signs = results
     r_results_fname = "spmi_r_pairwise_chis_result.csv"
     r_results_fpath = os.path.join(results_dirpath, r_results_fname)
     spmi_pairwise_chis_r = pd.DataFrame.from_csv(r_results_fpath)
     assert_allclose(spmi_pairwise_chis_r.values.astype(float),
                     spmi_pairwise_chis_python.values.astype(float))
+    np.testing.assert_(cellwise_signs.iat[0, 2] == 1)
 
 
 def test_SPMI_false_using_bonferroni():
@@ -666,7 +669,7 @@ def test_SPMI_false_using_bonferroni():
                                                          [columns_factor])
     test = multiple_response_table._test_SPMI_using_bonferroni
     result = test(rows_factor, columns_factor)
-    p_value_overall_bonferroni, cellwise_p_bonferroni_python = result
+    p_value_overall_bonferroni, cellwise_p_bonferroni_python, _ = result
     fpath = os.path.join(results_dirpath, "spmi_r_bonferroni.csv")
     spmi_bonferroni_r = pd.DataFrame.from_csv(fpath)
 
@@ -732,7 +735,7 @@ def test_multiple_mutual_independence_true():
     np.testing.assert_(rao_p_value >= 0.05)
     bonferroni_test = multiple_response_table._test_MMI_using_bonferroni
     bonferroni_p_value_overall, \
-    bonferroni_cell_p_values = bonferroni_test(srcv, mrcv)
+    bonferroni_cell_p_values, _ = bonferroni_test(srcv, mrcv)
     np.testing.assert_(bonferroni_p_value_overall >= 0.05)
     np.testing.assert_(np.all(bonferroni_cell_p_values >= 0.05))
 
@@ -757,7 +760,7 @@ def test_single_pairwise_mutual_independence_true():
     np.testing.assert_(rao_p_value >= 0.05)
     bonferroni_test = multiple_response_table._test_SPMI_using_bonferroni
     result = bonferroni_test(mrcv_1, mrcv_2)
-    bonferroni_p_value_overall, bonferroni_cell_p_values = result
+    bonferroni_p_value_overall, bonferroni_cell_p_values, _ = result
     np.testing.assert_(bonferroni_p_value_overall >= 0.05)
     np.testing.assert_(np.all(bonferroni_cell_p_values >= 0.05))
 
@@ -837,7 +840,7 @@ def test_MRCV_table_from_data():
     construct = ctab.MultipleResponseTable.from_data
     table = construct(multiple_response_questions, 5, 5)
     expected = np.array([44, 49, 15, 22, 12])  # from a manual run
-    np.testing.assert_equal(table.table.iloc[:, 0], expected)
+    np.testing.assert_equal(table.table.iloc[0, :], expected)
 
 
 def test_MRCV_table_from_factors():
@@ -850,7 +853,7 @@ def test_MRCV_table_from_factors():
     multiple_response_table = ctab.MultipleResponseTable([rows_factor, ],
                                                          [columns_factor])
     expected = np.array([44, 49, 15, 22, 12])  # from a manual run
-    np.testing.assert_equal(multiple_response_table.table.iloc[:, 0].values,
+    np.testing.assert_equal(multiple_response_table.table.iloc[0, :].values,
                             expected)
 
 
@@ -1088,36 +1091,38 @@ def test_combining_factors():
                          multiple_response=True)
 
     srcv_srcv = srcv_1.combine_with(srcv_2)
-    np.testing.assert_(srcv_srcv.labels[0] == ("motorcycle", "motorcycle"))
-    np.testing.assert_(srcv_srcv.labels[-1] == ("truck", "truck"))
+
+    np.testing.assert_(srcv_srcv.labels[0] ==
+                       "('motorcycle', 'motorcycle')")
+    np.testing.assert_(srcv_srcv.labels[-1] == "('truck', 'truck')")
     np.testing.assert_(srcv_srcv.data.shape == (1000, 9))
 
     srcv_mrcv = srcv_1.combine_with(mrcv_1)
-    np.testing.assert_(srcv_mrcv.labels[0] == ("motorcycle", "candy"))
-    np.testing.assert_(srcv_mrcv.labels[-1] == ("truck", "sushi"))
+    np.testing.assert_(srcv_mrcv.labels[0] == "('motorcycle', 'candy')")
+    np.testing.assert_(srcv_mrcv.labels[-1] == "('truck', 'sushi')")
     np.testing.assert_(srcv_mrcv.data.shape == (1000, 15))
 
     mrcv_mrcv = mrcv_1.combine_with(mrcv_2)
-    np.testing.assert_(mrcv_mrcv.labels[0] == ("candy", "English"))
-    np.testing.assert_(mrcv_mrcv.labels[-1] == ("sushi", "none"))
+    np.testing.assert_(mrcv_mrcv.labels[0] == "('candy', 'English')")
+    np.testing.assert_(mrcv_mrcv.labels[-1] == "('sushi', 'none')")
     np.testing.assert_(mrcv_mrcv.data.shape == (1000, 25))
 
     narrow = mrcv_2.cast_wide_to_narrow()
     wide_narrow = mrcv_1.combine_with(narrow)
-    np.testing.assert_(wide_narrow.labels[0] == ("candy", "English"))
-    np.testing.assert_(wide_narrow.labels[-1] == ("sushi", "none"))
+    np.testing.assert_(wide_narrow.labels[0] == "('candy', 'English')")
+    np.testing.assert_(wide_narrow.labels[-1] == "('sushi', 'none')")
     np.testing.assert_(wide_narrow.data.shape == (1000, 25))
 
     narrow_wide = narrow.combine_with(mrcv_1)
-    np.testing.assert_(narrow_wide.labels[0] == ("English", "candy",))
-    np.testing.assert_(narrow_wide.labels[-1] == ("none", "sushi",))
+    np.testing.assert_(narrow_wide.labels[0] == "('English', 'candy')")
+    np.testing.assert_(narrow_wide.labels[-1] == "('none', 'sushi')")
     np.testing.assert_(narrow_wide.data.shape == (1000, 25))
 
     narrow_2 = mrcv_2.cast_wide_to_narrow()
     narrow_narrow = narrow.combine_with(narrow_2)
     np.testing.assert_(narrow_narrow.labels[0] ==
-                       ("English", "English"))
-    np.testing.assert_(narrow_narrow.labels[-1] == ("none", "none"))
+                       "('English', 'English')")
+    np.testing.assert_(narrow_narrow.labels[-1] == "('none', 'none')")
     np.testing.assert_(narrow_narrow.data.shape == (1000, 25))
 
 